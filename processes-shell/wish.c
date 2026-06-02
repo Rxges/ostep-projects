eb69fe75a516fa90b3550a01cdbf27d871341642
Rxges <nuzhat.nuba@gmail.com>
2026-06-02 17:28:40 -0500

    correctly prints "wish> " in loop, basic commands work (ls, pwd), and exit feature added

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -9,83 +9,58 @@
 
 int main(int argc, char *argv[]) {
 
-    printf("wish> ");
+    while(1) {
+        printf("wish> ");
+        
+        char *line = NULL;
+        size_t len = 0;
+        
+        // ssize_t nread;
+        // while ((nread = getline(&line, &len, stdin)) != -1) {
+        //     // printf("wish> ");
+        //     printf("Retrieved line of length %zd:\n", nread);
+        // }
+        getline(&line, &len, stdin);
 
-    char *line = NULL;
-    size_t len = 0;
-    
-    // ssize_t nread;
-    // while ((nread = getline(&line, &len, stdin)) != -1) {}
-    getline(&line, &len, stdin);
+        char* cmd_argv[len];
+        const char *delim = ",; \n";
+        //char *strtok(char *str, const char *delim);
+        char* token = strtok(line, delim);
+        size_t index = 0;
+        while(token != NULL) {
+            // printf("token: %s\n", token);
+            
+            cmd_argv[index] = strdup(token);
+            // printf("cmd_argv[%ld]: %s\n", index, cmd_argv[index]);
+            index++;
+            // Subsequent calls: pass NULL to keep parsing the same string
+            token = strtok(NULL, delim); 
+            
+        }
+        cmd_argv[index] = NULL;
+        
+        free(line);
 
-    char* cmd_argv[len];
-    const char *delim = ",;/ ";
-    //char *strtok(char *str, const char *delim);
-    char* token = strtok(line, delim);
-    size_t index = 0;
-    while(token != NULL) {
-        cmd_argv[index] = token;
-        // printf("cmd_argv[%ld]: %s\n", index, cmd_argv[index]);
-        index++;
-        // Subsequent calls: pass NULL to keep parsing the same string
-        token = strtok(NULL, delim); 
+        if(strcmp(cmd_argv[0], "exit") == 0) {
+            break;
+        } else {
+            int rc = fork();
+            
+            // creates new process
+            if(rc == 0) {
+                // child
+                execvp(cmd_argv[0], cmd_argv);
+                printf("failed\n"); // if successful, doesn't return
+
+            } else if (rc > 0) {
+                // parent
+                (void) wait(NULL);
+                // printf("parent pid: %d, child pid: %d\n", (int)getpid(), rc);
+            } else {
+                // failure
+            }
+        }
     }
-    cmd_argv[index] = NULL;
-    free(line);
-
-
-    // execv(cmd_argv[0], cmd_argv);
-    // // int execv(const char *pathname, char *const argv[]);
-    // // if successful, doesn't return
-    // printf("failed\n");
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // int rc = fork();
-    // int x = 10;
-
-
-    // // creates new process
-    // if(rc == 0) {
-    //     // child
-    //     printf("child pid: %d %d\n", (int) getpid(), x);
-
-    //     char* cmd_argv[argc];
-    //     cmd_argv[0] = strdup("/bin/ls");
-    //     cmd_argv[1] = strdup("-l");
-    //     cmd_argv[2] = NULL;
-    
-    //     execv(cmd_argv[0], cmd_argv);
-    //     // int execv(const char *pathname, char *const argv[]);
-    //     // if successful, doesn't return
-    //     printf("failed\n");
-
-
-
-    // } else if (rc > 0) {
-    //     // parent
-    //     (void) wait(NULL);
-    //     printf("parent pid: %d, child pid: %d x: %d\n", (int)getpid(), rc, x);
-    // } else {
-    //     // failure
-    // }
 
     return 0;
 }
